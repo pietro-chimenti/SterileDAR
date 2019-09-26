a47a133eef669ae28a0a4f2cf58bb863ccea3f7f
RicardoBassi <rikardo_bassi@hotmail.com>
2019-09-26 01:00:11 -0300

    Update

--- SterileDar/OscillationModel.py
+++ SterileDar/OscillationModel.py
@@ -8,6 +8,7 @@ class OscillationModel:
 	DelM2 = 1.7 # eletron volts
 	Ue4_2   = 0.019
 	Umu4_2  = 0.015
+	Ut4_2	= 0.01
 	
 	def Pee(self,L,E):
 		return 1.-4.*(1-self.Ue4_2)*(self.Ue4_2)*((np.sin(1.27*self.DelM2*L/E))**2)
@@ -18,4 +19,8 @@ class OscillationModel:
 	def Pme(self,L,E):
 		return 4.*(self.Umu4_2)*(self.Ue4_2)*((np.sin(1.27*self.DelM2*L/E))**2)
 
-	
+	def Pmt(self,L,E):
+                return 4.*(self.Umu4_2)*(self.Ut4_2)*((np.sin(1.27*self.DelM2*L/E))**2)
+
+	def Pet(self,L,E):
+                return 4.*(self.Ue4_2)*(self.Ut4_2)*((np.sin(1.27*self.DelM2*L/E))**2)
